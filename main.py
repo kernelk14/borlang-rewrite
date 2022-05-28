1044e364da395f56055571ecaaa54ccbdfc7d921
kernelk14 <khyleisaias@gmail.com>
2022-05-28 09:01:56 +0800

    BR: Started development

---
+++ main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+""" This is a rewrite of the Borlang Programming Language.
+    My plan is to make Borlang a compiled language, but maybe 
+    I will transpile this into another compiled programming language. 
+"""
