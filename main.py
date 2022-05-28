40d467a9358a7009e4801c5d1975b8bd3be420f5
kernelk14 <khyleisaias@gmail.com>
2022-05-28 09:37:12 +0800

    BR: Add demo file and string slicing

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
-#<<<<<<< HEAD
 """ This is a rewrite of the Borlang Programming Language.
     My plan is to make Borlang a compiled language, but maybe 
     I will transpile this into another compiled programming language. 
 """
-#=======
-#print("Hello World")
-#>>>>>>> aca72bd75344bc3cd958130eaa62eecdc5b037ad
+stack = []
+
+filename = open("hello.bl", "r")
+program = filename.read().split()
+
+for p, op in enumerate(program):
+    print(program[::])
