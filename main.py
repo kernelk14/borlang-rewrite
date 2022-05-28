c6eb524d3bc54089ff556343c3002e4f259cec83
kernelk14 <khyleisaias@gmail.com>
2022-05-28 09:28:29 +0800

    BR: Fix repo issues

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-<<<<<<< HEAD
+#<<<<<<< HEAD
 """ This is a rewrite of the Borlang Programming Language.
     My plan is to make Borlang a compiled language, but maybe 
     I will transpile this into another compiled programming language. 
 """
-=======
-print("Hello World")
->>>>>>> aca72bd75344bc3cd958130eaa62eecdc5b037ad
+#=======
+#print("Hello World")
+#>>>>>>> aca72bd75344bc3cd958130eaa62eecdc5b037ad
