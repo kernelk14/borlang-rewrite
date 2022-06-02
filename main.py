f7362dceb45cf91312195faebc2ca674a95ed5cf
kernelk14 <khyleisaias@gmail.com>
2022-06-02 08:18:24 +0800

    BR: Added functions

--- main.py
+++ main.py
@@ -20,9 +20,11 @@ program = filename.read().split()
 
 # I need to choose a programming language to transpile to.
 # For now i chose Jakt(https://github.com/serenityos/jakt).
-out.write("function main(args: [String]) {\n")
+# out.write("function main(args: [String]) {\n")
 for p, op in enumerate(program):
     i = 1
+    if program[p] == 'fun':
+        out.write(f"function {program[p+1]}()\n")
     if program[p] == 'write':
         out.write('    println("{')
         out.write('}",')
@@ -30,5 +32,9 @@ for p, op in enumerate(program):
         # print("I saw a write statement")
     if program[p] == 'var':
         out.write(f'    let {program[p+1]} = {program[p+3]}\n')
-out.write("}\n")
+    if program[p] == 'do':
+        out.write("{")
+    if program[p] == 'end':
+        out.write("}\n")
+# out.write("}\n")
 # os.system(f"jakt {of}")
